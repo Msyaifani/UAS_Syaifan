32208a2d9707bbe57a9ff47d83fd34349fea8f4b
Msyaifani <84662125+Msyaifani@users.noreply.github.com>
2021-07-17 13:17:56 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/gitaIstrada/bot-jst-uas/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Msyaifani/UAS_Syaifan/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
